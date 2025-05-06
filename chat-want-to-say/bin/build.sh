034e492cf208a5a2272e7ee020354cdbc6ff2a5d
Xin Wang <yw987194828@gmail.com>
2025-05-06 16:58:45 +0800

    replace repo with tabao source and fix build.sh

--- chat-want-to-say/bin/build.sh
+++ chat-want-to-say/bin/build.sh
@@ -1,9 +1,20 @@
+# export BASE_PATH=$(cd `dirname $0`;pwd)
+# if [ ! $1 ]; then
+#     $1='production'
+# fi
+# # Build front with yarn.
+# cd $BASE_PATH/../
+# yarn
+# echo "yarn build --mode $1"
+# yarn build --mode $1
+
+
 export BASE_PATH=$(cd `dirname $0`;pwd)
-if [ ! $1 ]; then
-    $1='production'
-fi
+#if [ ! $1 ]; then
+#    $1='production'
+#fi
 # Build front with yarn.
 cd $BASE_PATH/../
 yarn
-echo "yarn build --mode $1"
-yarn build --mode $1
+#echo "yarn build --mode $1"
+yarn build --mode 'development'
